b9240a92ae03cd105b8bd901ef7b04fa7cd493c3
Dineshchandra47 <dineshchandraindia5@gmail.com.com>
2023-07-04 17:36:32 +0530

    Updated Project.jsx(#1)🐛

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { AiFillGithub, AiFillEye } from "react-icons/ai";
 
-
 const Project = (props) => {
   return (
     <div className="max-w-sm  border border-gray-200 rounded-lg shadow bg-[#101319] dark:border-gray-700">
@@ -14,9 +13,7 @@ const Project = (props) => {
             {props.title}
           </h5>
         </a>
-        <p className="mb-3 font-normal text-gray-400">
-          {props.desc}
-        </p>
+        <p className="mb-3 font-normal text-gray-400">{props.desc}</p>
         <div className=" flex gap-[1rem]">
           <a
             href={props.github}
@@ -27,15 +24,17 @@ const Project = (props) => {
             Github
             <AiFillGithub className=" w-[18px] h-[18px]" />
           </a>
-          {props.demo && <a
-            href={props.demo}
-            rel="noreferrer"
-            target="_blank"
-            className="inline-flex gap-[0.5rem] items-center px-3 py-2 text-sm font-medium text-center text-white rounded-md border hover:text-white border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 mr-2 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800"
-          >
-            Demo
-            <AiFillEye className="  w-[18px] h-[18px]" />
-          </a>}
+          {props.demo && (
+            <a
+              href={props.demo}
+              rel="noreferrer"
+              target="_blank"
+              className="inline-flex gap-[0.5rem] items-center px-3 py-2 text-sm font-medium text-center text-white rounded-md border hover:text-white border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 mr-2 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800"
+            >
+              Demo
+              <AiFillEye className="  w-[18px] h-[18px]" />
+            </a>
+          )}
         </div>
       </div>
     </div>
